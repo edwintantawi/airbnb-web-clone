9c2b585a5fec413a6c1e1b9c570b86627c6ffb14
edwintantawi <edwintantawi@gmail.com>
2021-08-05 17:44:02 +0700

    🚀 add AppHeader & AppHeaderMenu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,12 @@
+import React from 'react';
 import AppHead from '@/components/atoms/AppHead';
-import AppLogo from '@/components/atoms/AppLogo';
+import AppHeader from '@/components/molecules/AppHeader';
 
 export default function Home() {
   return (
-    <div className="p-3 text-center">
-      <AppHead
-        title="Airbnb: Vacation Rentals, Cabins, Beach Houses, Unique Homes & Experiences"
-        description="Find vacation rentals, cabins, beach houses, unique homes and experiences around the world - all made possible by hosts on Airbnb."
-      />
-      <AppLogo className="text-primary mx-auto" />
-      <h1 className="text-xl my-2">
-        <a
-          href="https://www.airbnb.com/"
-          target="__blank"
-          className="text-primary font-bold hover:underline"
-        >
-          Airbnb
-        </a>{' '}
-        Clone
-      </h1>
-    </div>
+    <>
+      <AppHead />
+      <AppHeader />
+    </>
   );
 }
