d240e28f8b4d38f933c48b38dedd78cd19dff684
edwintantawi <edwintantawi@gmail.com>
2021-08-05 17:42:26 +0700

    🔧 custom container & boxshadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,27 @@ module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  corePlugins: {
+    container: false,
+  },
   theme: {
     fontFamily: {
       sans: 'Roboto, Helvetica Neue, sans-serif',
+    },
+    container: {
+      center: true,
+    },
+    boxShadow: {
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none',
+      arround: '0 10px 28px rgba(0,0,0,0.2)',
     },
     colors: {
       transparent: 'transparent',
@@ -30,5 +48,27 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          margin: '0 auto',
+          padding: '0 16px',
+          '@screen sm': {
+            padding: '0 24px',
+          },
+          '@screen md': {
+            padding: '0 40px',
+          },
+          '@screen lg': {
+            padding: '0 72.5px',
+          },
+          '@screen xl': {
+            maxWidth: '1760px',
+          },
+        },
+      });
+    },
+  ],
 };
