7bc33f203a4ca77de8f5d4462ac8d864af560db4
edwintantawi <edwintantawi@gmail.com>
2021-08-06 17:45:03 +0700

    🚀 add AppHero

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 // components
 import AppHead from '@/components/atoms/AppHead';
 import AppHeader from '@/components/organisms/AppHeader';
+import AppHero from '@/components/atoms/AppHero';
 
 export default function Home() {
   return (
     <>
       <AppHead />
       <AppHeader />
+      <AppHero />
     </>
   );
 }
