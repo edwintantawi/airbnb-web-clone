757ad898405d6b1ca6b6180a5ce5b9e40fef2f8c
edwintantawi <edwintantawi@gmail.com>
2021-08-07 14:45:48 +0700

    🚀 add section

---
+++ components/atoms/AppSection.tsx
@@ -0,0 +1,14 @@
+const AppSection = ({ title, className, children }) => {
+  return (
+    <section className="my-10">
+      <div className="container">
+        <h2 className="mb-2 text-2xl font-bold md:mb-4 lg:mb-8 md:text-3xl lg:text-4xl">
+          {title}
+        </h2>
+        <div className={className}>{children}</div>
+      </div>
+    </section>
+  );
+};
+
+export default AppSection;
