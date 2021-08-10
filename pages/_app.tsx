9b18fedb6e9fcdbf0ad69ca3cd01b45a5f28742e
edwintantawi <edwintantawi@gmail.com>
2021-08-10 22:47:56 +0700

    🚀 add bar of progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import { ContextProvider } from 'context/store';
 import { AppProps } from 'next/app';
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
 // styles
 import '../styles/globals.css';
 import '../styles/reactDateRange.css';
+
+const progressBar = new ProgressBar({
+  size: 4,
+  color: '#FF385C',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progressBar.start);
+Router.events.on('routeChangeComplete', progressBar.finish);
+Router.events.on('routeChangeError', progressBar.finish);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
